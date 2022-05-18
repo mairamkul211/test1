60d0104053c8c5beebe920043eb7c778698f9af5
mairamkul211 <mairamkulkeldibekuulu76@gmail.com>
2022-05-19 01:08:34 +0300

    iso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect }  from 'react';
 import './App.css';
+import { useState } from 'react';
+import isoPhoto from './photo_2022-05-19_00-13-42.jpg'
 
 function App() {
+
+  const getTheme = () => {
+    return JSON.parse(localStorage.getItem("theme")) ||false 
+  }
+
+  const[theme, setTheme] = useState(getTheme());
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme))
+  }, [theme])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? "theme-dark": ""}>
+
+      <div className='content-bg-color main-content' >
+      <div className='dos'>
+        <h1>Dark Mode is</h1>
+      <input type='checkbox' onChange={() => setTheme(!theme)} checked={theme ? "true": ""}/>
+      </div>
+      <div className='block'>
+        <img className='iso' src={isoPhoto} alt=""photo/>
+        <h3>nasirovv_i</h3>
+      <a src="https://www.instagram.com/nasirovv_i/"><button className='a'>нажми меня</button></a>
+      </div>
+   
+      </div>
     </div>
   );
 }
